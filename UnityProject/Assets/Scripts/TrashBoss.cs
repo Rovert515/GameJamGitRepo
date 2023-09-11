48e1f12d5a6acd0a920f624cf0acdd03f083d3ee
maggie wong <magdalynwong@gmail.com>
2023-09-11 13:49:18 +0200

    trash spitting animation

--- UnityProject/Assets/Scripts/TrashBoss.cs
+++ UnityProject/Assets/Scripts/TrashBoss.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class TrashBoss : MonoBehaviour
 {
+
+    private Animator animator;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
+        animator.speed = 0.4f;
     }
 
     // Update is called once per frame
